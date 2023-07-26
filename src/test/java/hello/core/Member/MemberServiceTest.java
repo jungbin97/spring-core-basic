931bd54df84690f69d717699142d4b8033c10324
ego2 <ljb5054@gmail.com>
2023-07-26 22:50:55 +0900

    관심사의 분리

--- src/test/java/hello/core/Member/MemberServiceTest.java
+++ src/test/java/hello/core/Member/MemberServiceTest.java
@@ -1,16 +1,26 @@
 package hello.core.Member;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+
+    //    MemberService memberService = new MemberServiceImpl(memberRepository);
+    MemberService memberService;
+    @BeforeEach     // 테스트 실행전에 반드시 실행되는
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
     @Test
-    void join(){
+    void join() {
         //given
         Member member = new Member(1L, "memberA", Grade.VIP);
 
